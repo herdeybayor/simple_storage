2e9028b9d8fcb94eba1414ec7bcd72aaa3824c16
herdeybayor <shereefdeen4real@gmail.com>
2025-04-15 23:43:23 +0100

    functions

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,5 +2,13 @@
 pragma solidity 0.8.19;
 
 contract SimpleStorage {
-    
+    uint256 public favoriteNumber; // 0
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
 }
